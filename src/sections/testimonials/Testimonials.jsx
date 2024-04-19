865bf267c4526101d11698bc986c96102cbd4b6f
Eric Youngmin Song <dudals2900@gmail.com>
2024-04-18 21:36:05 -0400

    Swiper installed

--- src/sections/testimonials/Testimonials.jsx
+++ src/sections/testimonials/Testimonials.jsx
@@ -1,9 +1,46 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/autoplay';
+
+// import required modules
+import { Pagination } from 'swiper/modules';
+
+import testimonials from './data';
+import Testimonial from './Testimonial';
 import './testimonials.css';
 
 const Testimonials = () => {
   return (
     <section id="testimonials">
-      <h2>Testimonials</h2>
+      <h2>What My Client Say</h2>
+      <p>These are unbiased testimonials from some of my clients</p>
+      <div className="container">
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={30}
+          breakpoints={{
+            601: { slidesPerView: 2 },
+            1025: { slidesPerView: 3 },
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Pagination, Autoplay]}
+          autoplay={true}
+          className="mySwiper"
+        >
+          {testimonials.map((t) => (
+            <SwiperSlide key={t.id}>
+              <Testimonial testimonial={t} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </section>
   );
 };
