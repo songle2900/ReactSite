d386543225a09b22d5b1992885ab4a612f780ca3
Eric Youngmin Song <dudals2900@gmail.com>
2024-04-18 23:40:08 -0400

    Testimonial done

---
+++ src/sections/contact/data.js
@@ -0,0 +1,17 @@
+import {HiOutlineMail} from 'react-icons/hi'
+import {RiMessengerLine} from 'react-icons/ri'
+import {FaWhatsapp} from 'react-icons/fa'
+
+const data = [
+    {id: 1, icon: <HiOutlineMail/>, link: 'mailto:ysong0430@gmail.com'},
+    {id: 2, icon: <RiMessengerLine/>, link: 'https://www.messenger.com/t/youngmin.song.549'},
+    {id: 3, icon: <FaWhatsapp/>, link: 'https://wa.me/+13433338625'}
+]
+
+
+// alternative whatsApp link
+// https://wa.me/yournumber
+// https://api.whatsapp.com/send/?phone=%2B233557097546
+
+
+export default data
