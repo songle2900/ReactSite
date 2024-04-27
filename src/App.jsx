1dd67ef636eb701e3bcd8966287c0ea32800a638
Eric Youngmin Song <dudals2900@gmail.com>
2024-04-26 21:46:05 -0400

    Accessibility features - On progress

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import FAQs from './sections/faqs/FAQs';
 import Contact from './sections/contact/Contact';
 import Footer from './sections/footer/Footer';
 import FloatingNav from './sections/floating-nav/FloatingNav';
-import Modal from './components/Modal';
+import Theme from './theme/Theme';
 
 const App = () => {
   return (
@@ -22,7 +22,7 @@ const App = () => {
       <FAQs />
       <Contact />
       <Footer />
-      <Modal />
+      <Theme />
       {/* <FloatingNav /> */}
     </main>
   );
---
+++ src/theme/Theme.jsx
@@ -0,0 +1,37 @@
+import Modal from '../components/Modal';
+import PrimaryColor from './PrimaryColor';
+import { primaryColors, backgroundColors } from './data';
+import BackgroundColor from './BackgroundColor';
+import './theme.css';
+
+const Theme = () => {
+  return (
+    <Modal className="theme__modal">
+      <h3>Customize Colour</h3>
+      <small>
+        Change the primary and background colour to your preference.
+      </small>
+      <div className="theme__primary-wrapper">
+        <h5>Primary Colour</h5>
+        <div className="theme__primary_colors">
+          {primaryColors.map((pColor) => (
+            <PrimaryColor key={pColor.className} className={pColor.className} />
+          ))}
+        </div>
+      </div>
+      <div className="theme__background-wrapper">
+        <h5>Background Colour</h5>
+        <div className="theme__background-colors">
+          {backgroundColors.map((bColor) => (
+            <BackgroundColor
+              key={bColor.className}
+              className={bColor.className}
+            />
+          ))}
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
+export default Theme;
