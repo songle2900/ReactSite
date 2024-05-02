09f6c1f4de3c4376af62c5274b07e869b3ffcf0f
Eric Youngmin Song <dudals2900@gmail.com>
2024-05-01 21:09:45 -0400

    useEffect & localStorage set up

--- src/context/theme-context.js
+++ src/context/theme-context.js
@@ -3,14 +3,19 @@ import themeReducer from "./themeReducer";
 
 export const ThemeContext = createContext();
 
-const initialThemeState = {primary: 'color-1', background: 'bg-1'}
+const initialThemeState = JSON.parse(localStorage.getItem('themeSettings')) || {primary: 'color-1', background: 'bg-1'}
 
 export const ThemeProvider = ({children}) => {
   const [themeState, dispatchTheme] = useReducer(themeReducer, initialThemeState);
   const themeHandler = (buttonClassName) => {
     dispatchTheme({type: buttonClassName})
   }
-  console.log(themeState);
+  
+  // save theme setting to localStorage
+  useEffect(() => {
+    localStorage.setItem('themeSettings', JSON.stringify(themeState));
+  }, [themeState.primary, themeState.background])
+
   return <ThemeContext.Provider value={{themeState, themeHandler}}>{children}</ThemeContext.Provider>
 }
 
