4ea7b28c479fd0f889c79b952a7de614c008fc3a
Eric Youngmin Song <dudals2900@gmail.com>
2024-05-01 20:59:14 -0400

    Accessibility modal added

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,12 @@ import Contact from './sections/contact/Contact';
 import Footer from './sections/footer/Footer';
 import FloatingNav from './sections/floating-nav/FloatingNav';
 import Theme from './theme/Theme';
+import { useThemeContext } from './context/theme-context';
 
 const App = () => {
+  const { themeState } = useThemeContext();
   return (
-    <main>
+    <main className={`${themeState.primary} ${themeState.background}`}>
       <Navbar />
       <Header />
       <About />
