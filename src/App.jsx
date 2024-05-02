d0caa19726213c4158a0b3ec357e87e7d7df2c67
Eric Youngmin Song <dudals2900@gmail.com>
2024-05-01 21:23:20 -0400

    Scrollspy added

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ const App = () => {
       <Contact />
       <Footer />
       <Theme />
-      {/* <FloatingNav /> */}
+      <FloatingNav />
     </main>
   );
 };
--- src/sections/floating-nav/FloatingNav.jsx
+++ src/sections/floating-nav/FloatingNav.jsx
@@ -1,7 +1,22 @@
+import data from './data';
+import Scrollspy from 'react-scrollspy';
+import Nav from './Nav';
 import './floating-nav.css';
 
 const FloatingNav = () => {
-  return <section id="floating__nav"></section>;
+  return (
+    <ul id="floating__nav">
+      <Scrollspy
+        className="scrollspy"
+        items={['header', 'about', 'services', 'portfolio', 'contact']}
+        currentClassName="active"
+      >
+        {data.map((item) => (
+          <Nav key={item.id} item={item} />
+        ))}
+      </Scrollspy>
+    </ul>
+  );
 };
 
 export default FloatingNav;
